1e1f49062db9e3c9c7f22648b32a4505132676f7
Falouhiayoub <falouhiayoub9@gmail.com>
2026-02-08 15:01:09 +0100

    change the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -33,7 +33,7 @@ export function Navbar({ user }: NavbarProps) {
         description: 'Logged out successfully',
       });
 
-      router.push('/');
+      router.push('/login');
     } catch (error) {
       toast({
         title: 'Error',
